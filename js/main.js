4c0898093614087e1d0899cf2dce57f12a464fcb
Steve Yoo <steveyoo@Steves-MacBook-Pro.local>
2015-05-28 23:28:07 -0700

    added pages for silver and platinum

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function loadTopNav(){
 	document.write("            <\/symbol>");
 	document.write("            <use xlink:href=\"#icon-spinner2\"><\/use>");
 	document.write("        <\/svg>");
-	document.write("        <a href=\"wire2.html\">COINFLIP<\/a>");
+	document.write("        <a href=\"home.html\">COINFLIP<\/a>");
 	document.write("        <svg class=\"icon-cog\">");
 	document.write("            <symbol id=\"icon-cog\" viewBox=\"0 0 1024 1024\">");
 	document.write("                <title>cog<\/title>");
@@ -27,7 +27,7 @@ function loadTopNavPersist(){
 	document.write("            <\/symbol>");
 	document.write("            <use xlink:href=\"#icon-spinner2\"><\/use>");
 	document.write("        <\/svg>");
-	document.write("        <a href=\"wire2.html\">COINFLIP<\/a>");
+	document.write("        <a href=\"home.html\">COINFLIP<\/a>");
 	document.write("        <svg class=\"icon-cog\">");
 	document.write("            <symbol id=\"icon-cog\" viewBox=\"0 0 1024 1024\">");
 	document.write("                <title>cog<\/title>");
@@ -40,7 +40,7 @@ function loadTopNavPersist(){
 
 function loadSideNav(selected){
 	document.write("    <aside>");
-	document.write("        <a href=\"wire2.html\">");
+	document.write("        <a href=\"home.html\">");
 	if(selected == 0)
 		document.write("        <figure class='nav-selected'>");
 	else
@@ -57,7 +57,7 @@ function loadSideNav(selected){
 	document.write("            <figcaption>Home<\/figcaption>");
 	document.write("        <\/figure>       ");
 	document.write("        <\/a> ");
-	document.write("        <a href=\"wire3.html\">");
+	document.write("        <a href=\"gold.html\">");
 	if(selected == 1)
 		document.write("        <figure class='nav-selected'>");
 	else
@@ -66,7 +66,7 @@ function loadSideNav(selected){
 	document.write("            <figcaption>My Gold<\/figcaption>");
 	document.write("        <\/figure>       ");
 	document.write("        <\/a> ");
-	document.write("        <a href=\"wire3.html\">");
+	document.write("        <a href=\"silver.html\">");
 	if(selected == 2)
 		document.write("        <figure class='nav-selected'>");
 	else
@@ -75,7 +75,7 @@ function loadSideNav(selected){
 	document.write("            <figcaption>My Silver<\/figcaption>");
 	document.write("        <\/figure>       ");
 	document.write("        <\/a> ");
-	document.write("        <a href=\"wire3.html\">");
+	document.write("        <a href=\"platinum.html\">");
 	if(selected == 3)
 		document.write("        <figure class='nav-selected'>");
 	else
@@ -122,13 +122,13 @@ $(window).load(function() {
 	 *        GRAPHING         *
 	 *                         *
 	 * * * * * * * * * * * * * */
- 	// graph for wire2 page
+ 	// graph for home page
  	var drawGraph = function(){
  		var pointStroke = "rgba(255,255,255,0.6)";
  		var pointHighlightFill = "#fff";
  		var pointHighlightStroke = "#fff";
 
- 		if(page == "wire2.html") {
+ 		if(page == "home.html") {
  			var data = {
  				labels: ["January", "February", "March", "April", "May", "June", "July"],
  				datasets: [
@@ -253,7 +253,177 @@ $(window).load(function() {
 			var coinChart = new Chart(ctx).Line(data,options);
 			coinChart.update();
 		}
-		else if(page =="wire3.html"){
+		else if(page =="gold.html"){
+			var data = {
+				labels: ["January", "February", "March", "April", "May", "June", "July"],
+				datasets: [
+				{
+					label: "Gold Total",
+					fillColor: "rgba(104, 206, 222, 0.05)",
+					strokeColor: "#FF6D67",
+					pointColor: "#FF6D67",
+					pointStrokeColor: pointStroke,
+					pointHighlightFill: pointHighlightFill,
+					pointHighlightStroke: pointHighlightStroke,
+					data: [700,820,700,800,730,950,900]
+				},
+				{
+					label: "1oz Gold",
+					fillColor: "rgba(104, 206, 222, 0.05)",
+					strokeColor: "#9FFF98",
+					pointColor: "#9FFF98",
+					pointStrokeColor: pointStroke,
+					pointHighlightFill: pointHighlightFill,
+					pointHighlightStroke: pointHighlightStroke,
+					data: [100, 110, 120, 90, 102, 135, 115]
+				}
+				]
+			};
+
+			var options = {
+
+			    ///Boolean - Whether grid lines are shown across the chart
+			    scaleShowGridLines : true,
+
+			    //String - Colour of the grid lines
+			    scaleGridLineColor : "rgba(104, 206, 222, 0.1)",
+
+			    //Number - Width of the grid lines
+			    scaleGridLineWidth : 1,
+
+			    //Boolean - Whether to show horizontal lines (except X axis)
+			    scaleShowHorizontalLines: true,
+
+			    //Boolean - Whether to show vertical lines (except Y axis)
+			    scaleShowVerticalLines: true,
+
+			    //Boolean - Whether the line is curved between points
+			    bezierCurve : true,
+
+			    //Number - Tension of the bezier curve between points
+			    bezierCurveTension : 0.4,
+
+			    //Boolean - Whether to show a dot for each point
+			    pointDot : true,
+
+			    //Number - Radius of each point dot in pixels
+			    pointDotRadius : 4,
+
+			    //Number - Pixel width of point dot stroke
+			    pointDotStrokeWidth : 1,
+
+			    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+			    pointHitDetectionRadius : 20,
+
+			    //Boolean - Whether to show a stroke for datasets
+			    datasetStroke : true,
+
+			    //Number - Pixel width of dataset stroke
+			    datasetStrokeWidth : 2,
+
+			    //Boolean - Whether to fill the dataset with a colour
+			    datasetFill : true,
+
+			    //String - A legend template
+			    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
+
+			    responsive: true,
+
+			    maintainAspectRatio: false,
+
+
+			};
+
+			var ctx = document.getElementById("total-chart").getContext("2d");
+			var coinChart = new Chart(ctx).Line(data,options);
+			coinChart.update();
+		}
+		else if(page =="silver.html"){
+			var data = {
+				labels: ["January", "February", "March", "April", "May", "June", "July"],
+				datasets: [
+				{
+					label: "Gold Total",
+					fillColor: "rgba(104, 206, 222, 0.05)",
+					strokeColor: "#FF6D67",
+					pointColor: "#FF6D67",
+					pointStrokeColor: pointStroke,
+					pointHighlightFill: pointHighlightFill,
+					pointHighlightStroke: pointHighlightStroke,
+					data: [700,820,700,800,730,950,900]
+				},
+				{
+					label: "1oz Gold",
+					fillColor: "rgba(104, 206, 222, 0.05)",
+					strokeColor: "#9FFF98",
+					pointColor: "#9FFF98",
+					pointStrokeColor: pointStroke,
+					pointHighlightFill: pointHighlightFill,
+					pointHighlightStroke: pointHighlightStroke,
+					data: [100, 110, 120, 90, 102, 135, 115]
+				}
+				]
+			};
+
+			var options = {
+
+			    ///Boolean - Whether grid lines are shown across the chart
+			    scaleShowGridLines : true,
+
+			    //String - Colour of the grid lines
+			    scaleGridLineColor : "rgba(104, 206, 222, 0.1)",
+
+			    //Number - Width of the grid lines
+			    scaleGridLineWidth : 1,
+
+			    //Boolean - Whether to show horizontal lines (except X axis)
+			    scaleShowHorizontalLines: true,
+
+			    //Boolean - Whether to show vertical lines (except Y axis)
+			    scaleShowVerticalLines: true,
+
+			    //Boolean - Whether the line is curved between points
+			    bezierCurve : true,
+
+			    //Number - Tension of the bezier curve between points
+			    bezierCurveTension : 0.4,
+
+			    //Boolean - Whether to show a dot for each point
+			    pointDot : true,
+
+			    //Number - Radius of each point dot in pixels
+			    pointDotRadius : 4,
+
+			    //Number - Pixel width of point dot stroke
+			    pointDotStrokeWidth : 1,
+
+			    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+			    pointHitDetectionRadius : 20,
+
+			    //Boolean - Whether to show a stroke for datasets
+			    datasetStroke : true,
+
+			    //Number - Pixel width of dataset stroke
+			    datasetStrokeWidth : 2,
+
+			    //Boolean - Whether to fill the dataset with a colour
+			    datasetFill : true,
+
+			    //String - A legend template
+			    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
+
+			    responsive: true,
+
+			    maintainAspectRatio: false,
+
+
+			};
+
+			var ctx = document.getElementById("total-chart").getContext("2d");
+			var coinChart = new Chart(ctx).Line(data,options);
+			coinChart.update();
+		}
+		else if(page =="platinum.html"){
 			var data = {
 				labels: ["January", "February", "March", "April", "May", "June", "July"],
 				datasets: [
@@ -352,7 +522,7 @@ $(window).load(function() {
 	 	$('.graph-panel').removeClass('graph-panel-show');
 	 	$('.market-status').fadeIn(0);
 	 	$('.market-list').fadeIn(0);
-	 	if( page == "wire3.html")
+	 	if( page == "gold.html")
 	 		$('.my_stack').fadeIn(0);
 	 	$('.mtb-2').removeClass('mobile-toggle-selected');
 	 	$('.mtb-1').addClass('mobile-toggle-selected');
@@ -362,7 +532,7 @@ $(window).load(function() {
 	 $('.mtb-2').click(function(){
 	 	$('.market-status').fadeOut(0);
 	 	$('.market-list').fadeOut(0);
-	 	if( page == "wire3.html")
+	 	if( page == "gold.html")
 	 		$('.my_stack').fadeOut(0);
 	 	$('.mtb-1').removeClass('mobile-toggle-selected');
 	 	$('.mtb-2').addClass('mobile-toggle-selected');
@@ -378,7 +548,7 @@ $(window).load(function() {
 	 		$('.graph-panel').removeClass('graph-panel-show');
 	 		$('.market-status').fadeIn(0);
 	 		$('.market-list').fadeIn(0);
-	 		if( page == "wire3.html")
+	 		if( page == "gold.html")
 	 			$('.my_stack').fadeIn(0);
 	 		$('.mtb-2').removeClass('mobile-toggle-selected');
 	 		$('.mtb-1').addClass('mobile-toggle-selected');
